75fce052da728476f9bbdc60e07f4752c5425bd1
Alexey Svirski <77006434+alexey-svirski@users.noreply.github.com>
2021-02-16 16:10:15 +0300

    Add files via upload

---
+++ filter/Filter.js
@@ -0,0 +1,61 @@
+var Filter = React.createClass({
+  displayName: "Filter",
+
+  propTypes: {
+    stringArr: React.PropTypes.arrayOf(React.PropTypes.string).isRequired,
+  },
+
+  getInitialState: function () {
+    return {
+      isSorted: false,
+      currArr: this.props.stringArr,
+      inputText: "",
+    };
+  },
+
+  buttonClicked: function () {
+    this.setState({ inputText: "", currArr: this.props.stringArr, isSorted: false });
+  },
+
+  checkboxChecked: function () {
+      var copy1 = Object.assign([], this.props.stringArr);
+    if (!this.state.isSorted) 
+      this.setState({ isSorted: true, currArr: copy1.sort() });
+    else
+        this.setState({ isSorted: false, currArr: this.props.stringArr });
+  },
+
+  textChanged: function (EO) {
+    this.setState({ inputText: EO.target.value });
+  },
+
+  render: function () {
+    var newArr = this.state.currArr.filter(
+      (v) => v.indexOf(this.state.inputText) > -1
+    );
+
+    return React.DOM.div(
+      { className: "Filter" },
+      React.DOM.input({
+        type: "checkbox",
+        onClick: this.checkboxChecked,
+        checked: this.state.isSorted,
+      }),
+      React.DOM.input({
+        type: "text",
+        onChange: this.textChanged,
+        value: this.state.inputText,
+      }),
+      React.DOM.input({
+        type: "button",
+        value: "Сброс",
+        onClick: this.buttonClicked,
+      }),
+      React.DOM.br(),
+      React.DOM.select(
+        { className: "Select", size: this.props.stringArr.length },
+        newArr.map((v) => React.DOM.option({ label: v }))
+      )
+    );
+  },
+});
