6109782c090ab3a7de1e5a9b74e6ebb49d09ab2d
alexey-svirski <alexey.svirski@gmail.com>
2021-03-13 15:53:50 +0300

    iframe fix

--- youtube_link_parser/src/VideoList.js
+++ youtube_link_parser/src/VideoList.js
@@ -2,10 +2,30 @@ import React from "react";
 import InputField from "./InputField";
 
 class VideoList extends React.Component {
-
   state = {
     currentPage: 1,
-    arrToRender: []
+    arrToRender: [],
+  };
+
+  showIframe = (EO) => {
+    console.log(EO.target.id);
+    var newArr = [];
+    newArr = this.state.arrToRender.map((v) => {
+      if (v.key === EO.target.id) {
+        var src = "https://www.youtube.com/embed/" + EO.target.id;
+        v = (
+          <iframe
+            key={EO.target.id}
+            title={EO.target.alt}
+            src={src}
+            width="480"
+            height="36"
+          ></iframe>
+        );
+      }
+      return v;
+    });
+    this.setState({ arrToRender: newArr });
   };
 
   cbRequestFinished = async (recievedRequest) => {
@@ -17,28 +37,38 @@ class VideoList extends React.Component {
         { method: "get" }
       );
       var data = await response.json();
-      //console.log(data);
+      console.log(data);
       var arr = [];
       for (var i in data.items) {
-        var finalLink = "https://www.youtube.com/embed/" + data.items[i].id.videoId;
-        var iFrame = <iframe key={i} title={ data.items[i].snippet.title} src={finalLink}></iframe>
-      //console.log(iFrame);
-        arr.push(iFrame);
+        var imgURL = data.items[i].snippet.thumbnails.high.url;
+        var toKey =
+          data.items[i].id.videoId ||
+          data.items[i].id.palylistId ||
+          data.items[i].id.channelId;
+        var finalIMG = (
+          <img
+            key={toKey}
+            id={toKey}
+            src={imgURL}
+            alt={data.items[i].snippet.title}
+            onClick={this.showIframe}
+          ></img>
+        );
+        arr.push(finalIMG);
       }
-      this.setState({arrToRender: arr});
+      this.setState({ arrToRender: arr });
     } catch (error) {
       console.log(error);
     }
   };
 
   render() {
-    var finalList = (
+    return (
       <div>
         <InputField cbRequestFinished={this.cbRequestFinished}></InputField>
         {this.state.arrToRender}
       </div>
     );
-    return finalList;
   }
 }
 
